bfbe62ae5f01468b56616ae9fd225db85cd80e12
Mayumi Inoue <mayumi@manidesign.org>
2021-09-01 16:07:22 +0900

    太陽光実績ページを追加

---
+++ page.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+<?php if(have_posts()): while(have_posts()): the_post(); ?>
+<?php $slug = $post->post_name; ?>
+<div class="main-img-area <?php echo $slug; ?>" style="background-image: url(<?php if ( has_post_thumbnail() ) {
+	$image_id = get_post_thumbnail_id ();
+	$image_url = wp_get_attachment_image_src ($image_id, true);
+	echo $image_url[0];
+} else {
+	echo get_bloginfo( 'template_directory' ) . '/images/main_company.jpg';
+} ?>);" id="pagetop">
+  <h1 class="page-title"><?php the_title(); ?></h1>
+</div><!-- /.main-img-area -->
+
+<div class="page inner">
+
+<?php the_content(); ?>
+
+</div><!-- /.page -->
+
+<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
