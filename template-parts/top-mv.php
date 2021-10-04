7d828c0964106b795355b6dd1ae35f5a86c23d99
Mayumi Inoue <mayumi@manidesign.org>
2021-10-04 15:17:18 +0900

    トップページCMスライド表示

--- template-parts/top-mv.php
+++ template-parts/top-mv.php
@@ -7,7 +7,8 @@
 <div class="mv-area" id="pagetop">
   <h1><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/logo_mv.svg" alt="住まい工房まつやま"></h1>
   <div class="mv-images">
-    <div class="img" style="background-image: url(<?php echo esc_url( get_template_directory_uri() ); ?>/images/mv_img_02.jpg);"></div>
+  <div class="img" style="background-image: url(<?php echo esc_url( get_template_directory_uri() ); ?>/images/mv_img_06.jpg);"></div>  
+  <div class="img" style="background-image: url(<?php echo esc_url( get_template_directory_uri() ); ?>/images/mv_img_02.jpg);"></div>
     <div class="img" style="background-image: url(<?php echo esc_url( get_template_directory_uri() ); ?>/images/mv_img_03.jpg);"></div>
     <div class="img" style="background-image: url(<?php echo esc_url( get_template_directory_uri() ); ?>/images/mv_img_05.jpg);"></div>
     <div class="img" style="background-image: url(<?php echo esc_url( get_template_directory_uri() ); ?>/images/mv_img_04.jpg);"></div>
